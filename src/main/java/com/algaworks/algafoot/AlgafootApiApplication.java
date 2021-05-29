205dfe3e3cf0cef5b865c2addc2e2254fa8a7c35
valdinarvp <valdinar-pereira@hotmail.com>
2021-05-29 14:10:56 -0300

    Criação do Projeto

---
+++ src/main/java/com/algaworks/algafoot/AlgafootApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algafoot/AlgafootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
